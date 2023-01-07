42e4130d1cfefd1b8a789180622ac6231e5ab3ce
DESKTOP-PQ8SB5P\qkrwh <qkrwhdgur0319@gmail.com>
2023-01-07 18:41:36 +0900

    시트 로직 완성 다른거 필요한건 없어보임

--- sheetPage/app.js
+++ sheetPage/app.js
@@ -21,7 +21,8 @@ const player = {
 
 }
 
-
+document.getElementById("input_finished").addEventListener("click",inputCompleted);
+ 
 function inputCompleted() {
   player.characterName = document.getElementById("characterName").value;
   player.playerName=document.getElementById("playerName").value;
@@ -51,27 +52,55 @@ function inputCompleted() {
   console.log(player);
 }
 
+//최종점수저장하는곳
+let totalpoints=document.getElementById("totalpoints").value;
 
-
+//HP, MP, 스킬점수 업데이트하는곳
 function printScores()
 {
   document.getElementById("HP").value=player.HP;
   document.getElementById("MP").value=player.MP;
-
   document.getElementById("occupaionSkillScore").value=player.occupaionSkillScore;
   document.getElementById("interestSkillScore").value=player.interestSkillScore;
   document.getElementById("totalpoints").value=parseInt(player.occupaionSkillScore) + parseInt(player.interestSkillScore);
 }
 
+//점수 토탈해서 보너스점수 들어올떄마다 업데이트
+document.getElementById("bonusSkillScore").addEventListener("change", function(){document.getElementById("totalpoints").value=parseInt(document.getElementById("bonusSkillScore").value) + parseInt(player.occupaionSkillScore) + parseInt(player.interestSkillScore);
+totalpoints=document.getElementById("totalpoints").value;
+});
 
-document.getElementById("bonusSkillScore").addEventListener("change", function(){document.getElementById("totalpoints").value=parseInt(document.getElementById("bonusSkillScore").value) + parseInt(player.occupaionSkillScore) + parseInt(player.interestSkillScore)});
+// functions 배열 만들어서 반복으로 하나씩 이벤트 핸들러 넣어줄거임
+const playerFunctions = document.querySelectorAll(".function_table");
+
+// function 하나씩 접근해서 이벤트 핸들러 만들고 점수 감소까지 업데이트
+for(const playerFunction of playerFunctions){
+  playerFunction.querySelector(".normal").value = parseInt(playerFunction.querySelector(".input_score").value) + parseInt(playerFunction.querySelector(".defult").value);
+  playerFunction.querySelector(".hard").value = Math.floor(parseInt(playerFunction.querySelector(".normal").value)/2);
+  playerFunction.querySelector(".extreme").value = Math.floor(parseInt(playerFunction.querySelector(".normal").value)/5);
+
+  playerFunction.querySelector(".input_score").addEventListener("change", function(){
+    playerFunction.querySelector(".normal").value = parseInt(playerFunction.querySelector(".input_score").value) + parseInt(playerFunction.querySelector(".defult").value);
+    playerFunction.querySelector(".hard").value = Math.floor(parseInt(playerFunction.querySelector(".normal").value)/2);
+    playerFunction.querySelector(".extreme").value = Math.floor(parseInt(playerFunction.querySelector(".normal").value)/5);
+    
+    let tempScore = totalpoints; 
+    for(const playerFunction of playerFunctions){
+      tempScore-=playerFunction.querySelector(".input_score").value;
+      console.log(playerFunction.querySelector(".input_score").value);
+      console.log(tempScore);
+    }
+    document.getElementById("totalpoints").value = tempScore;
+  })
+}
 
 
 
 
-document.getElementById("input_finished").addEventListener("click",inputCompleted);
 
-console.log(document.querySelectorAll("#input_score"));
+
+
+
 /* document.getElementById("characterName").addEventListener("change", function(){player.characterName = document.getElementById("characterName").value;});
 */
 
